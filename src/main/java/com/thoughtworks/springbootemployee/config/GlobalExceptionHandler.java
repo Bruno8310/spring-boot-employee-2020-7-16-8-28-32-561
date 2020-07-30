90c4d029a6f79063434a90f303af5fd09124b354
userName <gongl3730@gmail.com>
2020-07-30 15:19:32 +0800

    [springboot Employee][Bruno/Hovees] feature: add GlobalExceptionHandler

---
+++ src/main/java/com/thoughtworks/springbootemployee/config/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.springbootemployee.config;
+
+import com.thoughtworks.springbootemployee.constant.ExceptionErrorMessage;
+import com.thoughtworks.springbootemployee.exception.IllegalOperationException;
+import com.thoughtworks.springbootemployee.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound() {
+        return ExceptionErrorMessage.NOT_FOUND.getErrorMessage();
+    }
+
+    @ExceptionHandler(IllegalOperationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String illegalOperate() {
+        return ExceptionErrorMessage.ILLEGAL_OPERATION.getErrorMessage();
+    }
+
+}
