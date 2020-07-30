e680e95496efbead4802194a67476f6c5ab9e93c
userName <gongl3730@gmail.com>
2020-07-30 11:52:49 +0800

    [springboot Employee][Bruno/Hovees] feature: add globalExceptionHandler class

---
+++ src/main/java/com/thoughtworks/springbootemployee/constant/ExceptionErrorMessage.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.springbootemployee.constant;
+
+public enum  ExceptionErrorMessage {
+
+    NOT_FOUND("Data Not Found!"),
+    ILLEGAL_OPERATION("Illegal Operation!");
+
+    public String errorMessage;
+
+    ExceptionErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+}
